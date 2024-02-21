e480ba3791a16bb2ec6f5d173bfcce4750b03b80
Karina Shupik <77788896h@gmail.com>
2024-02-21 21:46:24 +0200

    feat: default settings

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from "./http_server/index";
 
 const HTTP_PORT = 8181;
 
 console.log(`http://localhost:${HTTP_PORT}/`);
 httpServer.listen(HTTP_PORT);
+
