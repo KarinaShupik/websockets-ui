07bf41776df90e625f76503b044faf61054af654
Karina Shupik <77788896h@gmail.com>
2024-02-25 17:18:39 +0200

    feat: wsServer connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { httpServer } from "./http_server/index";
+import  wss  from './wsServer/wsServer';
 
 const HTTP_PORT = 8181;
 
 console.log(`http://localhost:${HTTP_PORT}/`);
 httpServer.listen(HTTP_PORT);
 
+httpServer.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
+});
--- src/wsServer/wsServer.ts
+++ src/wsServer/wsServer.ts
@@ -1,3 +1,9 @@
 import 'dotenv/config';
+import WebSocket from 'ws';
 
-const WS_PORT = process.env['PORT'];
+//const port = process.env['WS_PORT'];
+const WS_PORT = 3000
+
+const wss = new WebSocket.Server({ port: WS_PORT });
+
+export default wss;
